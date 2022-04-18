76ffcc72bf6f507d4a1e336f1c274a4114189c5c
Kousarbanu <56392981+kousarbanu@users.noreply.github.com>
2022-04-19 00:22:27 +0530

    Add files via upload

---
+++ Criminal_RM_Using_MVC/Criminal_RM_Using_MVC/ErrorsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Criminal_RM_Using_MVC.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult NoRecordsFound()
+        {
+            return View();
+        }
+        public ActionResult PageNotFoundError()
+        {
+            return View();
+        }
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+        public ActionResult UnauthorizedError()
+        {
+            return View();
+        }
+        public ActionResult GenericError()
+        {
+            return View();
+        }
+    }
+}
